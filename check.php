f14a704328a4648507c4c2cde90c16bcbf592490
sadeghpm <sadeghpm@gmail.com>
2023-11-28 08:25:01 +0330

    Refactor SSL monitoring and embed file content into the code
    The SSL monitoring script was significantly refactored for simplicity and maintainability.
    Previously, domain information was retrieved from a separate 'domain.php' file, and email report template was read from 'template.md'. Having these as physically separate entities added some complexity to the code and made it less easy to manage. Now, these data are embedded directly into the script ('check.php') as a constant and an environmental variable respectively. It makes code more streamlined and easier to manage.
    
    Also, updated the Github workflow file to adjust for the removal of 'domains.php' and add a condition to check if an SSL certificate is expired. This is to further refine the alert system, allowing for more granular and relevant alerts based on the certificate status.

--- check.php
+++ check.php
@@ -1,50 +1,79 @@
 <?php
 
-$domains = require_once __DIR__ . "/domains.php";
-$template = file_get_contents(__DIR__ . "/template.md");
-date_default_timezone_set('Asia/Tehran');
+const TEMPLATE = <<<TEMPLATE
+# SSL expiration monitoring
 
-function getSSLCertificate(string $domain)
+**Checked on %s**
+
+| Domain | Remained | Status       |
+|--------|----------|--------------|
+
+TEMPLATE;
+
+class SSLCertificateMonitor
 {
-    $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
-    $read = stream_socket_client("ssl://" . $domain . ":443", $errno, $errstr, 4, STREAM_CLIENT_CONNECT, $get);
 
-    if ($read === false) {
-        throw new Exception("Failed to connect: $errstr ($errno)");
+    private $expiryLimit;
+    private $domains;
+    private $template;
+
+    public function __construct()
+    {
+        $this->domains = explode(',', getenv('DOMAINS'));
+        $this->expiryLimit = getenv("EXPIRATION_LIMIT");
+        date_default_timezone_set('Asia/Tehran');
+        $this->template = sprintf(TEMPLATE, date('Y-m-d H:i'));
     }
 
-    $cert = stream_context_get_params($read);
-    return openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
-}
+    public function getSSLCertificate(string $domain)
+    {
+        $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+        $read = stream_socket_client("ssl://" . $domain . ":443", $errno, $errstr, 4, STREAM_CLIENT_CONNECT, $get);
 
-function getDaysTillExpiry(array $certinfo)
-{
-    $validTo = date('Y-m-d', $certinfo['validTo_time_t']);
-
-    $now = new DateTime();
-    $expiryDate = new DateTime($validTo);
-    $interval = $now->diff($expiryDate);
-
-    return $interval->format('%a');
-}
-
-$template = sprintf($template, date('Y-m-d H:i'));
-
-foreach ($domains as $domain) {
-    try {
-        $certinfo = getSSLCertificate($domain);
-        $daysTillExpiry = getDaysTillExpiry($certinfo);
-        $template .= sprintf("| %s     | %d Day   | %s |" . PHP_EOL, $domain, $daysTillExpiry, $daysTillExpiry > 10 ? "✅" : "❌");
-        if ($daysTillExpiry > 25) {
-            echo "The SSL certificate for " . $domain . " expires in " . $daysTillExpiry . " days" . PHP_EOL;
-        } else {
-            echo "WARNING! The SSL certificate for " . $domain . " has expired." . PHP_EOL;
+        if ($read === false) {
+            throw new Exception("Failed to connect: $errstr ($errno)");
         }
-    } catch (Exception $e) {
-        $template .= sprintf("| %s     | %s       | %s |" . PHP_EOL, $domain, $e->getMessage(), "❌");
-        echo "ERROR! May $domain has expired: " . $e->getMessage() . PHP_EOL;
+
+        $cert = stream_context_get_params($read);
+        return openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
     }
 
-    file_put_contents(__DIR__ . "/README.md", $template);
+    public function getDaysTillExpiry(array $certinfo)
+    {
+        $validTo = date('Y-m-d', $certinfo['validTo_time_t']);
+
+        $now = new DateTime();
+        $expiryDate = new DateTime($validTo);
+        $interval = $now->diff($expiryDate);
+
+        return $interval->format('%a');
+    }
+
+    public function monitor()
+    {
+        foreach ($this->domains as $domain) {
+            try {
+                $certinfo = $this->getSSLCertificate($domain);
+                $daysTillExpiry = $this->getDaysTillExpiry($certinfo);
+                $this->template .= sprintf("| %s     | %d Day   | %s |" . PHP_EOL, $domain, $daysTillExpiry, $daysTillExpiry > $this->expiryLimit ? "✅" : "❌");
+                if ($daysTillExpiry > $this->expiryLimit) {
+                    echo "The SSL certificate for " . $domain . " expires in " . $daysTillExpiry . " days" . PHP_EOL;
+                } else {
+                    if ($daysTillExpiry > 0) {
+                        echo "WARNING! The SSL certificate for " . $domain . " will be expired soon." . PHP_EOL;
+                    } else {
+                        echo "WARNING! The SSL certificate for " . $domain . " has expired." . PHP_EOL;
+                    }
+                }
+            } catch (Exception $e) {
+                $this->template .= sprintf("| %s     | %s       | %s |" . PHP_EOL, $domain, $e->getMessage(), "❌");
+                echo "ERROR! May $domain has expired: " . $e->getMessage() . PHP_EOL;
+            }
+
+            file_put_contents(__DIR__ . "/README.md", $this->template);
+        }
+    }
 }
-echo "DONE.";
+
+$monitor = new SSLCertificateMonitor();
+$monitor->monitor();
--- template.md
+++
@@ -1,6 +0,0 @@
-# SSL expiration monitoring
-
-**Checked on %s**
-
-| Domain | Remained | Status       |
-|--------|----------|--------------|
